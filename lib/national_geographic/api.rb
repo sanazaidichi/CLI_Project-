d127244570ce9db7a960b5757f54979456860caa
github username <github email address>
2020-03-21 00:29:31 -0500

    update

--- lib/national_geographic/api.rb
+++ lib/national_geographic/api.rb
@@ -1,8 +1,7 @@
 class API 
 def fetch(latest_stories)
     url = "http://newsapi.org/v2/everything?q=#{latest_stories}&from=2020-03-19&apiKey=0e3b22862264449ea652702249a5229b"
-    response = HTTParty.get(url)
-    binding.pry
-    response.parsed_response
+    response = HTTParty.get(url) 
+    response.parsed_response 
 end 
 end 
--- lib/national_geographic/cli.rb
+++ lib/national_geographic/cli.rb
@@ -6,11 +6,27 @@ class CLI
         puts "1. Science, 2. Travel, 3. History, & 4. Animals"
         input = gets.strip.downcase
         API.new.fetch(input)
-      show_articles
+       #show_articles 
+        case input 
+        when "science" 
+            puts "in science"   
+        when "travel" 
+            puts "in travel"
+        when "history" 
+            puts "in history"
+        when "animals" 
+            puts "in animals"
+        when "exit"
+            puts "Thanks for browsing National Geographic. Goodbye!"
+        else 
+            puts "invalid entry"
+         #they didn't put in a correct input. 
+        end
     end
 
-    def show_articles
-        
+    
+    def show_articles(url)
+         
     end
 end 
 
